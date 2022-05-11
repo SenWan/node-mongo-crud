d03d84da09b720e64bd87ba320a958aa168bca3a
U Sen Wan <senwan96@gmail.com>
2022-05-12 01:02:41 +0600

    db user insert

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const port = process.env.PORT || 5000;
 app.get(cors());
 app.use(express.json());
 
+// user == dbuser1
+// password == ikPtaHxI02kKV7mO
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://dbuser1:ikPtaHxI02kKV7mO@cluster0.5abec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        await client.connect();
+        const usersCollection = client.db('foodExpress').collection('user');
+        const user = {name: 'SenWin', email: 'senwin@gmail.com'};
+        const result = await usersCollection.insertOne(user);
+        console.log(`user inserted with id: ${result.insertedId}`)
+    }
+    finally{
+        //await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Running My Node CRUD Server');
